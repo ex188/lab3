e0e0a213d138cc743827a3a4a34c84e467b09c6d
ex188 <594718333@qq.com>
2023-04-19 17:47:28 -0700

    Reversed and tests change

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,16 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedShortArray() {
+    int[] input1 = {1,2,3 };
+    assertArrayEquals(new int[]{3,2,1 }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedLongArray() {
+    int[] input1 = {1,2,3,4,5,6,7,8,9,10 };
+    assertArrayEquals(new int[]{10,9,8,7,6,5,4,3,2,1 }, ArrayExamples.reversed(input1));
+  }
 }
